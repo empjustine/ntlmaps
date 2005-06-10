6f014e376f818118a495a9b53d54de009f33434b
dixond <dixond@4414eb6b-45f7-0310-abf5-9c8861e1d96f>
2005-06-10 03:56:57 +0000

    Add code from 0.9.9.1 release

--- trunk/lib/server.py
+++ trunk/lib/server.py
@@ -18,7 +18,7 @@
 #
 
 import socket, thread, sys, signal, getpass
-import proxy_client, www_client, monitor_upstream, ntlm_procs
+import proxy_client, monitor_upstream, ntlm_procs
 
 #--------------------------------------------------------------
 class AuthProxyServer:
@@ -31,6 +31,7 @@ class AuthProxyServer:
         self.sigLock = thread.allocate_lock() # For locking in the sigHandler
         self.monLock = thread.allocate_lock() # For keeping the monitor thread sane
         self.watchUpstream = 0
+        self.monitor = None
         if not self.config['NTLM_AUTH']['NTLM_TO_BASIC']:
             if not self.config['NTLM_AUTH']['PASSWORD']:
                 tries = 3
@@ -80,21 +81,7 @@ class AuthProxyServer:
 
     #--------------------------------------------------------------
     def client_run(self, conn, addr):
-        if self.config['GENERAL']['PARENT_PROXY']:
-            # working with MS Proxy
-            if self.watchUpstream:
-                # Locking here is really more of a 'nice to have';
-                # if performance suffers on heavy load we can trade
-                # drops here for drops on bad proxy later.
-                self.monLock.acquire()
-                c = proxy_client.proxy_HTTP_Client(conn, addr, self.config)
-                self.monitor.threadsToKill.append(c)
-                self.monLock.release()
-            else:
-                c = proxy_client.proxy_HTTP_Client(conn, addr, self.config)
-        else:
-            # working with MS IIS and any other
-            c = www_client.www_HTTP_Client(conn, addr, self.config)
+        c = proxy_client.proxy_HTTP_Client(conn, addr, self.config, self.watchUpstream, self.monLock, self.monitor.threadsToKill)
         thread.start_new_thread(c.run, ())
 
     #--------------------------------------------------------------
